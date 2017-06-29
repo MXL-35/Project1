7ddcac3d1e96575ef34ab4f72d0f7c417388f720
MXL-35 <1376574911@qq.com>
2017-06-29 11:22:21 +0800

    新建ExamActivity类

---
+++ app/src/main/java/com/example/admin1/myapplication1/Activity/ExamActivity.java
@@ -0,0 +1,19 @@
+package com.example.admin1.myapplication1.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.admin1.myapplication1.R;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class ExamActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exam_layout);
+    }
+}
